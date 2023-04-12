e3cef0138df4ddf97391ac7dfd58adc6e5ec13c5
bagass541 <balics@mail.ru>
2023-04-12 22:44:41 +0300

    add global exception and add 'add new employee' function

--- src/main/java/com/bagas/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/bagas/spring/rest/controller/MyRESTController.java
@@ -3,12 +3,19 @@ package com.bagas.spring.rest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bagas.spring.rest.entity.Employee;
+import com.bagas.spring.rest.exception_handling.EmployeeIncorrectData;
+import com.bagas.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.bagas.spring.rest.service.EmployeeService;
 
 
@@ -32,6 +39,37 @@ public class MyRESTController {
 	{
 		Employee employee = employeeService.getEmployee(id);
 		
+		if(employee == null)
+		{
+			throw new NoSuchEmployeeException("There is no employee with ID = " + id 
+					+ " int Database");
+		}
 		return employee;
 	}
+	
+	@PostMapping("/employees")
+	public Employee addNewEmployee(@RequestBody Employee employee)
+	{
+		employeeService.saveEmployee(employee);
+		return employee;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
+
+
---
+++ src/main/java/com/bagas/spring/rest/exception_handling/NoSuchEmployeeException.java
@@ -0,0 +1,10 @@
+package com.bagas.spring.rest.exception_handling;
+
+public class NoSuchEmployeeException extends RuntimeException {
+
+	public NoSuchEmployeeException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
